0198ac24ebaa4b7fa223aea71d528e29b14cc911
annuyadav125-debug <annuyadav125@gmail.com>
2026-03-03 13:39:50 +0530

    Add files via upload
    JS

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToAbout() {
+    document.getElementById("about").scrollIntoView({behavior: "smooth"});
+}
+
+function submitForm() {
+    alert("Thank you! Our team will contact you soon.");
+    return false;
+}
